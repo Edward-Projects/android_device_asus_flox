539a66d451691c3bb51c279cb55e58cd3bdba2fe
Edward Wu <bluehome.wu@gmail.com>
2021-12-02 20:30:37 +0800

    flox: Drop Lineage Livedisplay and trust HAL

--- device-common.mk
+++ device-common.mk
@@ -148,14 +148,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8960
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sysfs
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
